5ede6ad3e5e5c736b837592173b8119a5d029ab4
akakClavin <144984073+akakClavin@users.noreply.github.com>
2024-06-22 21:37:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.lalavia.dns-dynamic.net": dockerHub,
+  "quay.lalavia.dns-dynamic.net": "https://quay.io",
+  "gcr.lalavia.dns-dynamic.net": "https://gcr.io",
+  "k8s-gcr.lalavia.dns-dynamic.net": "https://k8s.gcr.io",
+  "k8s.lalavia.dns-dynamic.net": "https://registry.k8s.io",
+  "ghcr.lalavia.dns-dynamic.net": "https://ghcr.io",
+  "cloudsmith.lalavia.dns-dynamic.net": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.lalavia.dns-dynamic.net": dockerHub,
 };
 
 function routeByHosts(host) {
